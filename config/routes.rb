baa139dac3d79436098b4cb047bec29ef57d3431
Dusan Orlovic <duleorlovic@gmail.com>
2022-05-11 11:02:33 +0200

    Add sign_in_development_path

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "pages#index"
+  get 'sign-in-development/:id', to: 'pages#sign_in_development', as: :sign_in_development
 end
